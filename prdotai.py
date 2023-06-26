5c2f2cc00aa7cd72a6565cda35ec600eda9f7328
Matt Wong <matt.wong@asimov.com>
2023-06-26 04:26:46 +0000

    update model/tokens

--- prdotai.py
+++ prdotai.py
@@ -5,23 +5,24 @@ import sys
 import subprocess
 import openai
 
+
 def main():
     if len(sys.argv) != 2:
         print(f"Usage: {sys.argv[0]} branch1 ")
         sys.exit(1)
 
     branch1 = sys.argv[1]
-    
+
     # Assuming that the openai API key is stored in the OPENAI_API_KEY environment variable
-    openai.api_key = os.getenv('OPENAI_API_KEY')
-    
-    diff_command = ['git', 'diff', branch1]
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    diff_command = ["git", "diff", branch1]
     diff_output = subprocess.run(diff_command, capture_output=True, text=True)
-    
+
     if diff_output.returncode != 0:
         print("Error in git diff command")
         sys.exit(1)
-    
+
     # Initial prompt with the headers for the PR
     initial_prompt = (
         "We would like to create a Pull Request Description based on the git diff."
@@ -35,27 +36,25 @@ def main():
         " Are you looking for feedback in a specific area?\n\n"
     )
 
-    diff_text = initial_prompt + diff_output.stdout
-    chunks = split_prompt(diff_text, 32000)
+    diff_text = diff_output.stdout + initial_prompt
+    chunks = split_prompt(diff_text, 24000)
     pr_description = ""
 
     for i, chunk in enumerate(chunks):
         print(f"Sending chunk {i+1} to OpenAI API...")
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-0613",
-            messages=[{
-                'role': 'user', 
-                'content': chunk['content']
-            }]
+            model="gpt-4",
+            messages=[{"role": "user", "content": chunk["content"]}],
         )
 
-        response_text = response['choices'][0]['message']['content']
+        response_text = response["choices"][0]["message"]["content"]
         print(f"Response from OpenAI API for chunk {i+1}:")
         print(response_text)
         pr_description += response_text
 
-    print("Full PR description:")
+    print("\n\nFull PR description:\n\n")
     print(pr_description)
+
 
 def split_prompt(text, split_length):
     if split_length <= 0:
@@ -69,25 +68,29 @@ def split_prompt(text, split_length):
         end = min((i + 1) * split_length, len(text))
 
         if i == num_parts - 1:
-            part_msg = f'[START PART {i + 1}/{num_parts}]\n' 
-            part_msg += text[start:end] + f'\n[END PART {i + 1}/{num_parts}]'
-            part_msg += '\nALL PARTS SENT. Now you can continue processing the request.'
+            part_msg = f"[START PART {i + 1}/{num_parts}]\n"
+            part_msg += text[start:end] + f"\n[END PART {i + 1}/{num_parts}]"
+            part_msg += "\nALL PARTS SENT. Now you can continue processing the request."
         else:
             part_msg = (
-                f'Do not answer yet. This is just another part of the text I want to send you.'
+                f"Do not answer yet. This is just another part of the text I want to send you."
                 ' Just receive and acknowledge as "Part {i + 1}/{num_parts} received"'
-                ' and wait for the next part.\n[START PART {i + 1}/{num_parts}]\n'
-                + text[start:end] + f'\n[END PART {i + 1}/{num_parts}]'
-                '\nRemember not answering yet. Just acknowledge you received this part'
+                " and wait for the next part.\n[START PART {i + 1}/{num_parts}]\n"
+                + text[start:end]
+                + f"\n[END PART {i + 1}/{num_parts}]"
+                "\nRemember not answering yet. Just acknowledge you received this part"
                 ' with the message "Part {i + 1}/{num_parts} received" and wait for the next part.'
             )
 
-        file_data.append({
-            'name': f'split_{str(i + 1).zfill(3)}_of_{str(num_parts).zfill(3)}.txt',
-            'content': part_msg
-        })
+        file_data.append(
+            {
+                "name": f"split_{str(i + 1).zfill(3)}_of_{str(num_parts).zfill(3)}.txt",
+                "content": part_msg,
+            }
+        )
 
     return file_data
+
 
 if __name__ == "__main__":
     main()
